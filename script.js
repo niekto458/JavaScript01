fee8953c571b5f58bbe092f0565c5f1fba24c20f
niekto458 <tus.adam13@gmail.com>
2026-02-26 17:03:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function toggleShop() {
-  const products = document.getElementById("products");
+const button = document.getElementById("shopButton");
+const products = document.getElementById("products");
+
+button.addEventListener("click", function () {
   products.classList.toggle("hidden");
-}
+
+  if (products.classList.contains("hidden")) {
+    button.textContent = "View Products";
+  } else {
+    button.textContent = "Hide Products";
+  }
+});
