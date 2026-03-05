cc17e70e16cd0f94188f3bad0c068a3b1957a92c
niekto458 <tus.adam13@gmail.com>
2026-03-05 17:22:56 +0100

    i changed the web

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-function toggleShop() {
+let balance = 500;
 
-const shop = document.getElementById("shop");
+function playGame(){
 
-shop.classList.toggle("hidden");
+let bet = parseInt(document.getElementById("betAmount").value);
+let colorChoice = document.getElementById("color").value;
+
+if(bet > balance || bet <= 0){
+alert("Invalid bet amount!");
+return;
+}
+
+let random = Math.random();
+let resultColor;
+
+if(random < 0.5){
+resultColor = "red";
+}else{
+resultColor = "black";
+}
+
+let resultText = "Result: " + resultColor;
+
+if(colorChoice === resultColor){
+balance += bet;
+resultText += " — You WON $" + bet;
+}else{
+balance -= bet;
+resultText += " — You LOST $" + bet;
+}
+
+document.getElementById("balance").innerText = "Balance: $" + balance;
+document.getElementById("result").innerText = resultText;
+
+if(balance <= 0){
+document.getElementById("gameOver").classList.remove("hidden");
+}
+
+}
+
+function restartGame(){
+
+balance = 500;
+
+document.getElementById("balance").innerText = "Balance: $500";
+document.getElementById("result").innerText = "";
+
+document.getElementById("gameOver").classList.add("hidden");
 
 }
